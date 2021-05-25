aa115ff027021d92cf6faada7f3b8623f1898870
Mayur Wankhade <mayur.wankhade2@gmail.com>
2021-05-25 11:19:56 +0530

    [mayur] Refactor : daily wage feature added

---
+++ EmployeeWageComputation/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+    public class Employee
+    {
+        const int isPresent = 1;
+        public int empHour = 8;
+        public int empRatePerHour = 20;
+
+        public void Attendence() 
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isPresent)
+            {
+                int dailyWage = empHour * empRatePerHour;
+                Console.WriteLine("Per day salary = " + dailyWage);
+            }
+            else
+                Console.WriteLine("Per day salary = " + 0);
+        }
+
+    }
+}
